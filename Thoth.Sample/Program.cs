dc6d510e01041f152e6116acbfa8d18431eadcca
Hugo Lamounier <hugolamo@gmail.com>
2023-04-03 12:32:55 -0300

    Swagger

--- Thoth.Sample/Program.cs
+++ Thoth.Sample/Program.cs
@@ -17,12 +17,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
